e962a5df5011052ed7cf005f7947511a95181287
Alexandru <ap.tyto@gmail.com>
2024-02-05 20:28:46 +0100

    updated 02.02.24 21:51

--- src/test/java/com_phonebook_tests/CreateAccountTests.java
+++ src/test/java/com_phonebook_tests/CreateAccountTests.java
@@ -1,32 +1,53 @@
 package com_phonebook_tests;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class CreateAccountTests extends TestBase{
 
     @Test
-    public void createNewAccountPositiveTest(){
+    public void registerExistedUserNegativeTest(){
 
         // click on Login Link
-        driver.findElement(By.cssSelector("[href='/login']")).click();
+        click(By.cssSelector("[href='/login']"));
 
         // enter email
-        driver.findElement(By.name("email")).click();
-        driver.findElement(By.name("email")).clear();
-        driver.findElement(By.name("email")).sendKeys("monkys@info.com");
+        type(By.name("email"), "monkys@info.com");
 
         // enter password
-        driver.findElement(By.name("password")).click();
-        driver.findElement(By.name("password")).clear();
-        driver.findElement(By.name("password")).sendKeys("Money10000$");
+        type(By.name("password"), "Money10000$");
 
         // click on Registration button
-        driver.findElement(By.name("registration")).click();
+        click(By.name("registration"));
 
         // assert Sign Out button is present
-        Assert.assertTrue(isElementPresent(By.cssSelector("button")));
+        Assert.assertTrue(isAlertPresent());
+    }
+
+    public void type(By locator, String text) {
+        click(locator);
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    private void click(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    public boolean isAlertPresent() {
+        Alert alert = new WebDriverWait(driver, Duration.ofSeconds(20))
+                .until(ExpectedConditions.alertIsPresent());
+        if (alert == null) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
 }
---
+++ src/test/java/com_phonebook_tests/LoginTests.java
@@ -0,0 +1,19 @@
+package com_phonebook_tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase{
+
+    @Test
+    public void loginRegisteredUserPositiveTest(){
+        // click on Login link
+
+        // enter email
+
+        // enter password
+
+        // click on Login button
+
+        // Assert
+    }
+}
